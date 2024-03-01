e2f1d686a6117a2614a009a66faf63aeabb9bd28
Sai Harshitha Akkina <120376983+harshitha-2201@users.noreply.github.com>
2024-03-01 12:14:35 +0530

    Update Contact.jsx
    add

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -20,7 +20,7 @@ import { getImageUrl } from "../../utils";
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="www.linkedin.com/in/harshitha22">harshitha22</a>
+          <a href="https://www.linkedin.com/in/harshitha22/">harshitha22</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
